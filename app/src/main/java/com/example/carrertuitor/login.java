44efc4d11c77efdee1ce2ebd134ed62b4123e7bd
Prajwal G <prajwal.gkvk@gmail.com>
2023-02-11 20:21:06 +0530

    sample

--- app/src/main/java/com/example/carrertuitor/login.java
+++ app/src/main/java/com/example/carrertuitor/login.java
@@ -10,6 +10,8 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        hello
+
+        //hello
+
     }
 }
