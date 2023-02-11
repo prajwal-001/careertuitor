16f880e8e311e3315b76405c74cd348b787450fa
prajwal-001 <124917343+prajwal-001@users.noreply.github.com>
2023-02-11 21:26:43 +0530

    Update spscn.java

--- CarrerTuitor/app/src/main/java/com/example/carrertuitor/spscn.java
+++ CarrerTuitor/app/src/main/java/com/example/carrertuitor/spscn.java
@@ -1,14 +1,25 @@
-package com.example.carrertuitor;
+package com.example.splashscreendemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
-public class spscn extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
+    
+    public static int SPLASH_TIMER = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spscn);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnerable()  {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.SPLASH_TIMER);
     }
 }
