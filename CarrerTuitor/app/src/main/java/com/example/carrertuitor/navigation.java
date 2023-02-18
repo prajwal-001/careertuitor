bebba157c4b40a7ddc715818343b0aa78774c245
vikasism <116189666+vikasism@users.noreply.github.com>
2023-02-18 10:08:05 +0530

    Update navigation.java

--- CarrerTuitor/app/src/main/java/com/example/carrertuitor/navigation.java
+++ CarrerTuitor/app/src/main/java/com/example/carrertuitor/navigation.java
@@ -1,1 +1,158 @@
 
+package com.example.navigationdrawer:
+
+import androidx.annotation. NonNull;
+
+import androidx.appcompat.app.ActionBarDrawerToggle:
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar:
+
+import androidx.core.view. GravityCompat:
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+import android.view.MenuItem:
+
+import android.widget.Toast;
+
+import com.google.android.material.navigation. NavigationView;
+
+public class MainActivity extends AppCompatActivity implements
+
+NavigationView.OnNavigationItemSelectedListener (
+
+private DrawerLayout drawerLayout;
+
+@Override
+
+protected void onCreate(Bundle savedInstanceState) [
+
+super.onCreate(savedInstanceState);
+
+setContentView(R.layout.activity_main);
+
+Toolbar toolbar findViewById(R.id.toolbar); //Ignore red line errors
+
+setSupportActionBar(toolbar);
+
+drawerLayout = findViewById(R.id.drawer_layout);
+
+NavigationView navigationView findViewById(R.id.nav_view);
+
+navigationView.setNavigationItemSelectedListener(this);
+
+R.string.open_nav,
+
+ActionBarDrawerToggle toggle new ActionBarDrawerToggle(this, drawerLayout,
+
+toolbar,
+
+R.string.close_nav);
+
+drawerLayout.addDrawerListener(toggle);
+
+toggle.syncState();
+
+if (savedInstanceState == null) {
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment container,
+
+HomeFragment()).commit(18)
+
+new
+toggle.syncState();
+
+if (savedInstanceState == null) {
+
+getSupportFragmentManager().beginTransaction().replace(R.id, fragment_container, new
+
+HomeFragment()).commit();
+
+navigationView.setCheckedItem(R.id.nav_home);
+
+@Override
+
+public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+switch (item.getItemId()) {
+
+case R.id.nav_home:
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container.
+
+new Home Fragment()).commit();
+
+break;
+
+case R.id.nav_settings:
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment container,
+
+new SettingsFragment()).commit();
+
+break;
+
+case R.id.nav_share:
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+
+new ShareFragment()).commit();
+
+break;
+
+case R.id.nav_about:
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment-container,
+
+new About Fragment()).commit();
+
+break;
+
+case R.id.nav-logout:
+
+Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+
+break;
+
+drawerLayout.closeDrawer(GravityCompat.START);
+
+return true;
+case R.id.nav_about:
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+
+new AboutFragment()).commit();
+
+break;
+
+case R.id.nav-logout:
+
+Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+
+break;
+
+}
+
+drawerLayout.closeDrawer(GravityCompat.START);
+
+return true;
+
+}
+
+@Override
+
+public void onBackPressed() {
+
+if (drawerLayout.isDrawerOpen (GravityCompat.START)) {
+
+drawerLayout.closeDrawer(GravityCompat. START);
+
+} else [
+
+super.onBackPressed();
+
+}
